42c67465a5d72d9b8904bcdbe1d9083910158970
Sushant Garudkar <garudkar.sa@gmail.com>
2022-11-28 15:23:50 +0530

    Minor Cleanup

--- utils/addProduct/utils.js
+++ utils/addProduct/utils.js
@@ -1,14 +1,13 @@
 export function validProductData(body) {
     // Check if body present
-    if (!body) return false;
+    if (!body) return false
 
     // check if passed body is object ({})
-    if (!(typeof body === 'object' && !Array.isArray(body) && body !== null)) return false;
+    if (!(typeof body === 'object' && !Array.isArray(body) && body !== null)) return false
 
     // check if data types are correct with valid value for them
-    'galaxy s20 '
-    if (!((typeof body.name === 'string' && body.name.trim().length > 1) && (typeof body.make === 'string' && body.make.trim().length > 3) && (typeof body.image === 'string' && body.image.trim().length > 10) && (typeof body.description === 'string' && body.description.trim().length > 300) && (typeof body.price === 'number' && body.price >= 0) && (typeof body.quantity === 'number' && body.quantity >= 1))) return false;
-    
+    if (!((typeof body.name === 'string' && body.name.trim().length > 1) && (typeof body.make === 'string' && body.make.trim().length > 3) && (typeof body.image === 'string' && body.image.trim().length > 10) && (typeof body.description === 'string' && body.description.trim().length > 300) && (typeof body.price === 'number' && body.price >= 0) && (typeof body.quantity === 'number' && body.quantity >= 1))) return false
+
     // product is valid
-    return true;
+    return true
 }
