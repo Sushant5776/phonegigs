539260106dd808cbb1d84a0edddbd9fe60fef6ed
garudkarshubham <garudkarshubham2989@gmail.com>
2022-12-26 17:38:28 +0530

    update
    Co-authored-by: Sushant Garudkar <Sushant5776@users.noreply.github.com>

--- pages/api/getProduct.js
+++ pages/api/getProduct.js
@@ -2,14 +2,9 @@ import { adminDBApp } from '../../utils/db'
 
 export default async function getProduct(req, res) {
 	if (!(req.method === 'GET')) return res.status(400).json({ message: 'Method Not Allowed!' })
-
 	if (!req.query.id) return res.status(400).json({ message: 'insufficient data to fetch product details!' })
 
 	const productDetails = await adminDBApp.firestore().doc(`/products/${ req.query.id }`).get()
-
-	console.log(`id match: ${ req.query.id === productDetails.id }`)
-	console.log(productDetails.data())
-
 
 	return res.status(200).json({ id: productDetails.id, ...productDetails.data() })
 }
